eba9ab13f1c23435da089243fe14b71f84155115
吕途 <buzhidaolvtu@163.com>
2018-11-17 23:21:28 +0800

    init

---
+++ src/main/java/org/opensource/analysis/CallGraph.java
@@ -0,0 +1,26 @@
+package org.opensource.analysis;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr4.java.JavaLexer;
+import org.antlr4.java.JavaParser;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class CallGraph {
+
+    public static void main(String[] args) throws IOException {
+        String sFile = "/Users/lvtu/workspace/java-callgraph/src/main/java/org/opensource/analysis/CallGraph.java";
+        CharStream cs = CharStreams.fromFileName(sFile, Charset.forName("utf-8"));
+        JavaLexer lexer = new JavaLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JavaParser parser = new JavaParser(tokens);
+        JavaParser.CompilationUnitContext tree = parser.compilationUnit(); // parse a compilationUnit
+        int hashCode = cs.hashCode();
+        System.out.println(hashCode);
+        MethodVisitor methodVisitor = new MethodVisitor();
+        tree.accept(methodVisitor);
+    }
+}
---
+++ src/main/java/org/opensource/analysis/MethodVisitor.java
@@ -0,0 +1,88 @@
+package org.opensource.analysis;
+
+import org.antlr4.java.JavaBaseVisitor;
+import org.antlr4.java.JavaParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MethodVisitor extends JavaBaseVisitor {
+
+    private final static Logger logger = LoggerFactory.getLogger(MethodVisitor.class);
+
+    @Override
+    public Object visitMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
+        logger.info("visitMethodDeclaration:{}", ctx.getText());
+        return super.visitMethodDeclaration(ctx);
+    }
+
+    @Override
+    public Object visitMethodModifier(JavaParser.MethodModifierContext ctx) {
+        return super.visitMethodModifier(ctx);
+    }
+
+    @Override
+    public Object visitMethodHeader(JavaParser.MethodHeaderContext ctx) {
+        logger.info("visitMethodHeader:{}", ctx.getText());
+        return super.visitMethodHeader(ctx);
+    }
+
+    @Override
+    public Object visitMethodName(JavaParser.MethodNameContext ctx) {
+        return super.visitMethodName(ctx);
+    }
+
+    @Override
+    public Object visitMethodDeclarator(JavaParser.MethodDeclaratorContext ctx) {
+        return super.visitMethodDeclarator(ctx);
+    }
+
+    @Override
+    public Object visitMethodBody(JavaParser.MethodBodyContext ctx) {
+        logger.info("visitMethodBody:{}", ctx.getText());
+        return super.visitMethodBody(ctx);
+    }
+
+    @Override
+    public Object visitMethodInvocation(JavaParser.MethodInvocationContext ctx) {
+        logger.info("visitMethodInvocation:{}", ctx.getText());
+        return super.visitMethodInvocation(ctx);
+    }
+
+    @Override
+    public Object visitInterfaceMethodDeclaration(JavaParser.InterfaceMethodDeclarationContext ctx) {
+        return super.visitInterfaceMethodDeclaration(ctx);
+    }
+
+    @Override
+    public Object visitInterfaceMethodModifier(JavaParser.InterfaceMethodModifierContext ctx) {
+        return super.visitInterfaceMethodModifier(ctx);
+    }
+
+    @Override
+    public Object visitMethodReference(JavaParser.MethodReferenceContext ctx) {
+        logger.info("visitMethodReference:{}", ctx.getText());
+        return super.visitMethodReference(ctx);
+    }
+
+    @Override
+    public Object visitMethodReference_lf_primary(JavaParser.MethodReference_lf_primaryContext ctx) {
+        return super.visitMethodReference_lf_primary(ctx);
+    }
+
+    @Override
+    public Object visitMethodReference_lfno_primary(JavaParser.MethodReference_lfno_primaryContext ctx) {
+        return super.visitMethodReference_lfno_primary(ctx);
+    }
+
+    @Override
+    public Object visitMethodInvocation_lf_primary(JavaParser.MethodInvocation_lf_primaryContext ctx) {
+        return super.visitMethodInvocation_lf_primary(ctx);
+    }
+
+    @Override
+    public Object visitMethodInvocation_lfno_primary(JavaParser.MethodInvocation_lfno_primaryContext ctx) {
+        return super.visitMethodInvocation_lfno_primary(ctx);
+    }
+
+
+}
