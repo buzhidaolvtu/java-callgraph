221c997128a320d1c19a932589468f90ec1cbbb5
吕途 <buzhidaolvtu@163.com>
2019-01-02 22:44:34 +0800

    call graph

--- src/main/java/org/opensource/analysis/algorithms/CasPerformance.java
+++ src/main/java/org/opensource/analysis/algorithms/CasPerformance.java
@@ -1,6 +1,11 @@
 package org.opensource.analysis.algorithms;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * 原子命令性能评估
@@ -10,16 +15,20 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class CasPerformance {
 
-    public static void main(String[] args) {
-        test();
-        long start = System.currentTimeMillis();
-        AtomicInteger atom = new AtomicInteger();
-        for (int i = 0; i < 10000 * 10000; i++) {
-            atom.incrementAndGet();
-        }
-        long end = System.currentTimeMillis();
-        System.out.println(end - start + "ms");
-        test();
+
+    public static void main(String[] args) throws InterruptedException {
+//        test();
+//        long start = System.currentTimeMillis();
+//        AtomicInteger atom = new AtomicInteger();
+//        for (int i = 0; i < 10000 * 10000; i++) {
+//            atom.incrementAndGet();
+//        }
+//        long end = System.currentTimeMillis();
+//        System.out.println(end - start + "ms");
+//        test();
+        test2();
+//        test3();
+        test4();
     }
 
     public static void test() {
@@ -31,5 +40,47 @@ public class CasPerformance {
         long end = System.currentTimeMillis();
         System.out.println(local);
         System.out.println(end - start + "ms");
+    }
+
+    /**
+     *
+     */
+    public static void test2() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(64);
+        LongAdder base = new LongAdder();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 2000 * 10000; i++) {
+            executorService.submit(base::increment);
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.HOURS);
+        long end = System.currentTimeMillis();
+        System.out.println("LongAdder ：" + (end - start));
+    }
+
+    public static void test3() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(500);
+        AtomicInteger base = new AtomicInteger();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 2000 * 10000; i++) {
+            executorService.submit(base::incrementAndGet);
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.HOURS);
+        long end = System.currentTimeMillis();
+        System.out.println("AtomicInteger ：" + (end - start));
+    }
+
+    public static void test4() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(64);
+        AtomicLong base = new AtomicLong();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 2000 * 10000; i++) {
+            executorService.submit(base::incrementAndGet);
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.HOURS);
+        long end = System.currentTimeMillis();
+        System.out.println("AtomicLong ：" + (end - start));
     }
 }
